27c8300c543eaa34d1c32ed2a61408e2aa9870a4
icebro <potatotot98@gmail.com>
2025-01-19 15:46:24 -0700

    RAHHHHHHHHHH

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -1,18 +1,22 @@
-onmousemove = function(e){
+var r = document.querySelector(':root');
+
+onmousemove = function(e) {
 	let x = e.clientX + 1 + "px";
 	let y = e.clientY + 1 + "px";
 
 	xx = e.clientX - 35 + "px";
 	yy = e.clientY - 60 + "px";
 	//^is used for kabeewm.js but since you cant change onmousemove twice i had to shove it here 
-	var r = document.querySelector(':root');
+
+
+
 	
 	function mouse1(){
 		r.style.setProperty('--mouseposy', y);
 		r.style.setProperty('--mouseposx', x );
 	}
 	setTimeout(mouse1, 0);
-	
+
 	
 	function mouse2(){
 		r.style.setProperty('--mouseposy1', y);
@@ -33,15 +37,36 @@ onmousemove = function(e){
 		r.style.setProperty('--mouseposx3', x );
 	}
 	setTimeout(mouse4, 105);
-	
-	
+
+
 	function mouse5(){
 		r.style.setProperty('--mouseposy4', y);
 		r.style.setProperty('--mouseposx4', x );
+		setTimeout(mouse6, 400);
+
 	}
 	setTimeout(mouse5, 140);
-	
+
+	function mouse6(){
+		if (r.style.getPropertyValue('--mouseposx4') === (r.style.getPropertyValue('--mouseposx'))) {
+			x = "9999px";
+			y = "9999px";
+			r.style.setProperty('--mouseposy', y);
+			r.style.setProperty('--mouseposx', x );
+			r.style.setProperty('--mouseposy1', y);
+			r.style.setProperty('--mouseposx1', x );
+			r.style.setProperty('--mouseposy2', y);
+			r.style.setProperty('--mouseposx2', x );
+			r.style.setProperty('--mouseposy3', y);
+			r.style.setProperty('--mouseposx3', x );
+			r.style.setProperty('--mouseposy4', y);
+			r.style.setProperty('--mouseposx4', x );
+		}
+	}
+
 	
 	
 	//console.log("mouse location:", x, y);
 }
+
+
