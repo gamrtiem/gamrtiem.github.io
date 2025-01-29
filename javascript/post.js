a31ea4a5aa2f692f6b8a86abf2c805b55c690979
icebro <potatotot98@gmail.com>
2025-01-29 00:16:35 -0700

    thigns arew nice now

--- javascript/post.js
+++ javascript/post.js
@@ -1,18 +1,4 @@
-let b = document.getElementById("behindstatus");
-const h = document.getElementById("statuscafe");
-console.log(b)
-function outputsize() {
-    const l = document.getElementById("statuscafe");
 
-    var r = document.querySelector(':root');
-    r.style.setProperty('--statusHeight', (l.offsetHeight + 5) + "px");
-
-    console.log(l.style.height)
-    console.log(l.style.width)
-    console.log(l.style)
-    console.log(l)
-    console.log(l.offsetHeight)
-}
-outputsize()
-
-new ResizeObserver(outputsize).observe(h)
+$("#topsect").load("top.html");
+$("#header").load("head.html");
+$("#status").load("status.html");
