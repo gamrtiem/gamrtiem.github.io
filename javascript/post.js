8ffb7f25ea58881e0adba96758c251b2a7a026b6
icebro <potatotot98@gmail.com>
2025-01-29 18:18:38 -0700

    ghj

--- javascript/post.js
+++ javascript/post.js
@@ -1,3 +1,3 @@
-$("#topsect").load("./2014-3-4-top.html");
-$("#header").load("./2014-3-4-head.html");
-$("#status").load("./2014-3-4-status.html");
+$("#topsect").load("../top");
+$("#header").load("../head");
+$("#status").load("../status");
