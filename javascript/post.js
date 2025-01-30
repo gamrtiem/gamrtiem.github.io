06201ce88d39015514c969785ff9a49a6d8496c5
icebro <potatotot98@gmail.com>
2025-01-30 01:28:02 -0700

    uhh maybe ?

--- javascript/post.js
+++ javascript/post.js
@@ -1,3 +1,10 @@
-$("#header").load("../head");
-$("#status").load("../status");
-$("#topsect").load("../top");
+
+$(document).ready(function() {
+    $("#header").load("../head");
+    $("#status").load("../status");
+    $("#topsect").load("../top");
+
+    $("#header").load("./2014-3-4-head.html");
+    $("#status").load("./2014-3-4-status.html");
+    $("#topsect").load("./2014-3-4-top.html");
+});
