1a19edafa6e23c0aa0e3282dd460ddbf33c1c7fb
icebro <potatotot98@gmail.com>
2025-01-31 20:11:10 -0700

    testb stuf

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -6,67 +6,78 @@ onmousemove = function(e) {
 
 	xx = e.clientX - 35 + "px";
 	yy = e.clientY - 60 + "px";
-	//^is used for kabeewm.js but since you cant change onmousemove twice i had to shove it here 
+	//^is used for kabeewm.js but since you cant change onmousemove twice i had to shove it here
 
 
 
-	
-	function mouse1(){
-		r.style.setProperty('--mouseposy', y);
-		r.style.setProperty('--mouseposx', x );
-	}
-	setTimeout(mouse1, 0);
-
-	
-	function mouse2(){
-		r.style.setProperty('--mouseposy1', y);
-		r.style.setProperty('--mouseposx1', x );
-	}
-	setTimeout(mouse2, 35);
-	
-	
-	function mouse3(){
-		r.style.setProperty('--mouseposy2', y);
-		r.style.setProperty('--mouseposx2', x );
-	}
-	setTimeout(mouse3, 70);
-	
-	
-	function mouse4(){
-		r.style.setProperty('--mouseposy3', y);
-		r.style.setProperty('--mouseposx3', x );
-	}
-	setTimeout(mouse4, 105);
-
-
-	function mouse5(){
-		r.style.setProperty('--mouseposy4', y);
-		r.style.setProperty('--mouseposx4', x );
-		setTimeout(mouse6, 400);
-
-	}
-	setTimeout(mouse5, 140);
-
-	function mouse6(){
-		if (r.style.getPropertyValue('--mouseposx4') === (r.style.getPropertyValue('--mouseposx'))) {
-			x = "9999px";
-			y = "9999px";
-			r.style.setProperty('--mouseposy', y);
-			r.style.setProperty('--mouseposx', x );
-			r.style.setProperty('--mouseposy1', y);
-			r.style.setProperty('--mouseposx1', x );
-			r.style.setProperty('--mouseposy2', y);
-			r.style.setProperty('--mouseposx2', x );
-			r.style.setProperty('--mouseposy3', y);
-			r.style.setProperty('--mouseposx3', x );
-			r.style.setProperty('--mouseposy4', y);
-			r.style.setProperty('--mouseposx4', x );
+	console.log("mouse location:", x, y);
+		if(!isNaN(e.pageX) && !isNaN(e.pageY)) {
+			rainbow(x, y)
 		}
-	}
+}
 
-	
-	
-	//console.log("mouse location:", x, y);
+rainbow = function (x, y) {
+
+		console.log(x + " " + y)
+
+		function mouse1() {
+			r.style.setProperty('--mouseposy', y);
+			r.style.setProperty('--mouseposx', x);
+		}
+
+		setTimeout(mouse1, 0);
+
+
+		function mouse2() {
+			r.style.setProperty('--mouseposy1', y);
+			r.style.setProperty('--mouseposx1', x);
+		}
+
+		setTimeout(mouse2, 35);
+
+
+		function mouse3() {
+			r.style.setProperty('--mouseposy2', y);
+			r.style.setProperty('--mouseposx2', x);
+		}
+
+		setTimeout(mouse3, 70);
+
+
+		function mouse4() {
+			r.style.setProperty('--mouseposy3', y);
+			r.style.setProperty('--mouseposx3', x);
+		}
+
+		setTimeout(mouse4, 105);
+
+
+		function mouse5() {
+			r.style.setProperty('--mouseposy4', y);
+			r.style.setProperty('--mouseposx4', x);
+			setTimeout(mouse6, 400);
+
+		}
+
+		setTimeout(mouse5, 140);
+
+		function mouse6() {
+			if (r.style.getPropertyValue('--mouseposx4') === (r.style.getPropertyValue('--mouseposx'))) {
+				x = "9999px";
+				y = "9999px";
+				r.style.setProperty('--mouseposy', y);
+				r.style.setProperty('--mouseposx', x);
+				r.style.setProperty('--mouseposy1', y);
+				r.style.setProperty('--mouseposx1', x);
+				r.style.setProperty('--mouseposy2', y);
+				r.style.setProperty('--mouseposx2', x);
+				r.style.setProperty('--mouseposy3', y);
+				r.style.setProperty('--mouseposx3', x);
+				r.style.setProperty('--mouseposy4', y);
+				r.style.setProperty('--mouseposx4', x);
+			}
+		}
+
 }
 
 
